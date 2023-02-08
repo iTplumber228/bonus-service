8ad793a17ac07e1362c03d3277f90b5d34a62025
iTplumber228 <121715161+iTplumber228@users.noreply.github.com>
2023-02-08 11:11:41 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,12 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
     
-    long expected = 30;
-    long amount = 1000_60;
+  long amount = 1000_60;
     boolean registered = true;
 
-    long actual = service.calculate(amount, registered);
-    boolean passed = expected == actual;
-    System.out.println(passed);
+    service.calculate(amount, registered);
   }
 }
+
+
 
